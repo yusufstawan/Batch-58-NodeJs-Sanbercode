9e9b234cec26b36d0fd4a0e8acb7bc7ea713b826
yusufstawan <yusufsetiyawan45@gmail.com>
2024-07-23 16:57:27 +0700

    update

--- tugas7-middleware-express/src/route.ts
+++ tugas7-middleware-express/src/route.ts
@@ -1,10 +1,21 @@
 import express from "express";
 
 import { single, multiple } from "./middlewares/upload.middleware";
+import cloudinary from "./utils/cloudinary";
+import { handleUpload } from "./utils/cloudinary";
 
 const router = express.Router();
 
-router.get("/upload/single", single, (req, res) => {});
+router.post("/upload/single", single, async (req, res) => {
+  const file = req.file as any;
+  handleUpload(file.path)
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((error) => {
+      res.send(error);
+    });
+});
 router.get("/upload/multiple", multiple, (req, res) => {});
 
 export default router;
--- tugas7-middleware-express/src/server.ts
+++ tugas7-middleware-express/src/server.ts
@@ -1,7 +1,13 @@
 import express from "express";
+import cors from "cors";
+import router from "./route";
 
 const app = express();
 const PORT = 3000;
+
+app.use(express.json());
+app.use(cors());
+app.use(router);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
