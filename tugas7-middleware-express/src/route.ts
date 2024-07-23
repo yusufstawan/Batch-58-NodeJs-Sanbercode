549ae077996aa442bef17c9d4ce614b505ac5bfe
yusufstawan <yusufsetiyawan45@gmail.com>
2024-07-23 21:49:17 +0700

    upload sementara

--- tugas7-middleware-express/src/route.ts
+++ tugas7-middleware-express/src/route.ts
@@ -1,21 +1,26 @@
 import express from "express";
 
 import { single, multiple } from "./middlewares/upload.middleware";
-import cloudinary from "./utils/cloudinary";
 import { handleUpload } from "./utils/cloudinary";
 
 const router = express.Router();
 
 router.post("/upload/single", single, async (req, res) => {
-  const file = req.file as any;
-  handleUpload(file.path)
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((error) => {
-      res.send(error);
-    });
+  const file = req.file;
+  if (!file) {
+    return res.status(400).send("Please upload a file");
+  }
+  const result = await handleUpload(file.path);
+  res.send(result);
 });
-router.get("/upload/multiple", multiple, (req, res) => {});
+
+router.post("/upload/multiple", multiple, (req, res) => {
+  const files: any = req.files;
+  if (!files) {
+    return res.status(400).send("Please upload a file");
+  }
+  const result = files.map((file: { path: string }) => handleUpload(file.path));
+  res.send(result);
+});
 
 export default router;
